f89e3d34d751db60053f8f9992fedcbf0203a2ea
Isaac Pei <peix@mskcc.org>
2019-01-22 11:32:00 -0500

    add delmh_summary to the Makefile

--- summary/delmh_summary.mk
+++ summary/delmh_summary.mk
@@ -4,22 +4,21 @@ include modules/Makefile.inc
 
 LOGDIR ?= log/delmh_summary.$(NOW)
 PHONY += delmh_summary
-
-HOTSPOT ?= $(wildcard $(foreach sample,$(SAMPLES),hotspot/$(sample).txt))
+DEFAULT_ENV = $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.4
 
 delmh_summary : tsv/delmh_summary.tsv
 
 # step 3: summary of filtered delmh
 tsv/delmh_summary.tsv : tsv/delmh_filtered.tsv
-	$(call RUN,-n 1 -s 4G -m 4G,"$(RSCRIPT) modules/summary/delmh_summary.R")
+	$(call RUN,-n 1 -s 8G -m 8G -v $$(DEFAULT_ENV),"$(RSCRIPT) modules/summary/delmh_summary.R")
 
 # step 2: filter delmh
 tsv/delmh_filtered.tsv : tsv/delmh_found.tsv
-	$(call RUN,-n 1 -s 4G -m 4G,"$(RSCRIPT) modules/summary/delmh_filter.R")
+	$(call RUN,-n 1 -s 8G -m 8G -v $$(DEFAULT_ENV),"$(RSCRIPT) modules/summary/delmh_filter.R")
 
 # step 1: calculate delmh
 tsv/delmh_found.tsv : tsv/all.somatic_indels.tsv
-	$(call RUN,-n 1 -s 4G -m 4G,"python modules/summary/calc_delmh.py")
+	$(call RUN,-n 1 -s 8G -m 8G -v $$(DEFAULT_ENV),"python modules/summary/calc_delmh.py")
 
 .DELETE_ON_ERROR:
 .SECONDARY:
