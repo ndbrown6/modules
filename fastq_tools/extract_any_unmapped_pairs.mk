fe0d8391e4102c88977c74cdfc6f5171d7e492bf
Isaac Pei <peix@mskcc.org>
2019-02-21 10:47:29 -0500

    fixed! - extract_any_unmapped_pairs.mk

--- fastq_tools/extract_any_unmapped_pairs.mk
+++ fastq_tools/extract_any_unmapped_pairs.mk
@@ -5,15 +5,15 @@ LOGDIR ?= log/extract_any_unmapped_pairs.$(NOW)
 
 .DELETE_ON_ERROR:
 .SECONDARY:
-.PHONY: extract_unmapped_pairs
+.PHONY: extract_any_unmapped_pairs
 
 VPATH = bam
 JAVA = $(HOME)/share/usr/jdk1.8.0_121/bin/java
 PICARD = /lila/data/riazlab/lib/src/picard.jar
 
-extract_any_unmapped_pairs : $(foreach sample,$(SAMPLES),extracted_reads/any_unmapped_pairs/$(sample)_1.fastq.gz)
+extract_any_unmapped_pairs : $(foreach sample,$(SAMPLES),extracted_reads/any_unmapped_pairs/fastq/$(sample)_1.fastq.gz)
 
-define extract-unmapped-pairs
+define extract-any-unmapped-pairs
 ## 1. Extract the unique read ID from all the unmmaped_reads
 extracted_reads/unmapped_pairs/%.txt: unmapped_reads/%.bam
 	$$(call RUN,-c -n 1 -s 4G -m 9G,"$(SAMTOOLS2) view $$< | cut -f1 | sort | uniq > extracted_reads/unmapped_pairs/$$*.txt")
@@ -24,11 +24,8 @@ extracted_reads/any_unmapped_pairs/%.bam : extracted_reads/unmapped_pairs/%.txt
 		READ_LIST_FILE=extracted_reads/unmapped_pairs/$$*.txt FILTER=includeReadList")
 
 ## 3. Convert the extracted bam files to fastq.gz format
-extracted_reads/any_unmapped_pairs/%_1.fastq.gz extracted_reads/any_unmapped_pairs/%_2.fastq.gz : extracted_reads/any_unmapped_pairs/%.bam
+extracted_reads/any_unmapped_pairs/fastq/%_1.fastq.gz extracted_reads/any_unmapped_pairs/fastq/%_2.fastq.gz : extracted_reads/any_unmapped_pairs/%.bam
 	$$(call RUN,-n 4 -s 4G -m 9G,"$(SAMTOOLS2) sort -T $$(<D)/$$* -O bam -n -@ 4 -m 6G $$< | $(SAMTOOLS2) fastq -f 1 -1 >(gzip -c > extracted_reads/any_unmapped_pairs/fastq/$$*.1.fastq.gz) -2 >(gzip -c > extracted_reads/any_unmapped_pairs/fastq/$$*.2.fastq.gz) -")
-
-	################## Previous code to convert to fastq
-	# $$(call RUN,-n 4 -s 4G -m 9G,"bamToFastq -i $$< -fq extracted_reads/unmapped_pairs/$$*_1.fastq -fq2 extracted_reads/unmapped_pairs/$$*_2.fastq")
 endef
 $(foreach pair,$(SAMPLES),\
-		$(eval $(call extract-unmapped-pairs,$sample)))
+		$(eval $(call extract-any-unmapped-pairs,$sample)))
