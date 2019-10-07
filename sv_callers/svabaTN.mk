0c9799ec7d10a6a0396c1b256e3baf8a1ab3fd4e
Isaac Pei <peix@mskcc.org>
2019-10-07 14:54:38 -0400

    update svabaTN.mk to run with 16 threads and 7200mins

--- sv_callers/svabaTN.mk
+++ sv_callers/svabaTN.mk
@@ -8,7 +8,7 @@ LOGDIR ?= log/svabaTN.$(NOW)
 .PHONY: svabaTN
 
 VPATH = bam
-CORES=8 ## set any number of cores
+CORES=16 ## set any number of cores
 #SVABAREF=$(HOME)/share/reference/GATK_bundle/2.3/human_g1k_v37.fasta
 SVABAREF=$(REF_FASTA)
 DBSNP=/data/riazlab/lib/reference/svaba/dbsnp_indel.vcf
@@ -17,7 +17,7 @@ svabaTN : $(foreach pair,$(SAMPLE_PAIRS),svaba/$(pair).svaba.somatic.indel.vcf)
 
 define svaba-tumor-normal
 svaba/$1_$2.svaba.somatic.indel.vcf : bam/$1.bam bam/$2.bam
-	$$(call RUN,-c -n 8 -s 12G -m 14G -w 3600,"svaba run -t bam/$1.bam -n bam/$2.bam -p $$(CORES) -D $$(DBSNP) -L 100000 -x 25000 -k /data/riazlab/lib/reference/wgs_blacklist_meres.bed -a $1_$2 -G $$(SVABAREF) && mkdir -p svaba && mv *svaba*.vcf svaba/")
+	$$(call RUN,-c -n 16 -s 12G -m 14G -w 7200,"svaba run -t bam/$1.bam -n bam/$2.bam -p $$(CORES) -D $$(DBSNP) -L 100000 -x 25000 -k /data/riazlab/lib/reference/wgs_blacklist_meres.bed -a $1_$2 -G $$(SVABAREF) && mkdir -p svaba && mv *svaba*.vcf svaba/")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
 		$(eval $(call svaba-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
