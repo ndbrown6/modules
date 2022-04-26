c09086a72a8c59389f80c2e406718e9b2f5fb385
David Brown <ndbrown6@users.noreply.github.com>
2022-04-26 00:28:42 -0400

    Update svabaTN.mk
    @ipstone Cleaned up your Makfile
    1/ You probably have to define where svaba is installed. Probably in a conda environment...
    2/ You can also replace line #34 with a `/some/path/to/svaba --help > version/svabaTN.txt` or `/some/path/to/svaba --help &> version/svabaTN.txt`

--- sv_callers/svabaTN.mk
+++ sv_callers/svabaTN.mk
@@ -1,23 +1,37 @@
-## Makefile to run https://github.com/walaj/svaba
-
 include modules/Makefile.inc
-LOGDIR ?= log/svabaTN.$(NOW)
 
-.DELETE_ON_ERROR:
-.SECONDARY:
-.PHONY: svabaTN
+LOGDIR = log/svabaTN.$(NOW)
 
-VPATH = bam
-CORES=16 ## set any number of cores
-#SVABAREF=$(HOME)/share/reference/GATK_bundle/2.3/human_g1k_v37.fasta
-SVABAREF=$(REF_FASTA)
-DBSNP=/data/riazlab/lib/reference/svaba/dbsnp_indel.vcf
+SVABA_CORES ?= 8
+SVABA_MEM_CORE ?= 6G
+SVABA_REF ?= $(REF_FASTA)
+SVABA_DBSNP ?= /data/riazlab/lib/reference/svaba/dbsnp_indel.vcf
+SVABA_BLACKLIST ?= /data/riazlab/lib/reference/wgs_blacklist_meres.bed
 
 svabaTN : $(foreach pair,$(SAMPLE_PAIRS),svaba/$(pair).svaba.somatic.indel.vcf)
 
 define svaba-tumor-normal
 svaba/$1_$2.svaba.somatic.indel.vcf : bam/$1.bam bam/$2.bam
-	$$(call RUN,-c -n 16 -s 4G -m 6G -w 7200,"svaba run -t bam/$1.bam -n bam/$2.bam -p $$(CORES) -D $$(DBSNP) -L 100000 -x 25000 -k /data/riazlab/lib/reference/wgs_blacklist_meres.bed -a $1_$2 -G $$(SVABAREF) && mkdir -p svaba && mv *svaba*.vcf svaba/")
+	$$(call RUN,-c -n $(SVABA_CORES) -s 4G -m $(SVABA_MEM_CORE) -w 72:00:00,"set -o pipefail && \
+										 mkdir -p svaba && \
+										 cd svaba && \
+										 svaba run \
+										 -t ../bam/$1.bam \
+										 -n ../bam/$2.bam \
+										 -p $$(SVABA_CORES) \
+										 -D $$(SVABA_DBSNP) \
+										 -L 100000 \
+										 -x 25000 \
+										 -k $$(SVABA_BLACKLIST) \
+										 -a $1_$2 \
+										 -G $$(SVABA_REF)")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
 		$(eval $(call svaba-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+
+
+..DUMMY := $(shell mkdir -p version; \
+	     touch version/svabaTN.txt)
+.SECONDARY:
+.DELETE_ON_ERROR:
+.PHONY: svabaTN
